5290dbbb159f3f514e6b427003621ae406ae25c0
LilyWS <stranger1212ss23@gmail.com>
2021-08-30 23:19:38 -0400

    finsished api and render system

---
+++ script.js
@@ -0,0 +1,73 @@
+var APIKey = "69466bb6e55e7142ed8c817a4a2917b9";
+var searchForm = document.querySelector
+var city = "Camden";
+var displayEl = document.querySelector("#display");
+var currentWeatherEl = document.querySelector("#currentWeather");
+var forecastEl = document.querySelector("#forecast");
+var searchURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`;
+var search5URL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIKey}`;
+var today = moment().format("YYYY-MM-DD");
+
+console.log(moment(today).add(1, 'days').format("YYYY-MM-DD"));
+
+function getWeather(url) {
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (data) {
+        console.log(data);
+        displayWeather(data);
+    });
+}
+
+function getForecastWeather(url) {
+    fetch(url)
+    .then(function (response) {
+        return response.json();
+      })
+    .then(function (data) {
+        console.log(data);
+        displayForecastWeather(data);
+    });
+}
+
+function displayWeather(data) {
+    currentWeatherEl.querySelector("h2").innerHTML = `${data.name} ${moment().format("M/D/YYYY")} <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>` ;
+
+    let dataQueue = [`Temp: ${data.main.temp}°F`, `Wind: ${data.wind.speed}MPH`, `Humidity: ${data.main.humidity}%`, `UV Index: ${data.main.humidity}%`]
+
+    for(let i=0;i<currentWeatherEl.querySelector("ul").children.length; i++){
+        let liEl = currentWeatherEl.querySelector("ul").children[i];
+        liEl.textContent = dataQueue[i];
+    }
+}
+
+function displayForecastWeather(data) {
+    let list = data.list;
+    let cards = forecastEl.querySelectorAll(".forecastCard");
+
+    let fCastDay = moment(today).add(1, 'days').format("YYYY-MM-DD");
+
+    for(let x=0;x<cards.length; x++){
+        for(let y=0;y<list.length; y++){
+            if(list[y].dt_txt==`${fCastDay} 15:00:00`){
+                cards[x].children[0].textContent = moment(fCastDay).format("M/DD/YY");
+                console.log(list[y])
+                let dataQueue = [`<img src="http://openweathermap.org/img/wn/${list[y].weather[0].icon}@2x.png" style="width:3em; height:auto;"/>`, `Temp: ${list[y].main.temp}°F`, `Wind: ${list[y].wind.speed}MPH`, `Humidity: ${list[y].main.humidity}%`]
+
+                for(let i=0;i<cards[x].children[1].children.length;i++){
+                    let liEl = cards[x].children[1].children[i];
+                    liEl.innerHTML = dataQueue[i];
+
+                }
+                
+            
+            }
+        }
+        fCastDay = moment(fCastDay).add(1, 'days').format("YYYY-MM-DD");
+    }
+}
+
+getWeather(searchURL);
+getForecastWeather(search5URL);
